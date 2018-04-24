54f65af5ae8bac73b8072ed7858e516b9b0ba593
Fabio Ebner <fabioebner@gmail.com>
2018-04-23 21:15:27 -0300

    removendo tipo de instituicao, mais dados na tabela e formas de pagamento

--- src/main/resources/db/migration/public/V1_2__C_Instituicao.sql
+++ src/main/resources/db/migration/public/V1_2__C_Instituicao.sql
@@ -1,15 +1,37 @@
 CREATE TABLE tb_instituicao(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(250) NOT NULL,
-    tipo_instituicao_id BIGINT REFERENCES tb_tipo_instituicao(id),
+    cep VARCHAR(250) NOT NULL,
+    logradouro VARCHAR(250) NOT NULL,
+    numero VARCHAR(250) NOT NULL,
+    complemento VARCHAR(250),
+    bairro VARCHAR(250) NOT NULL,
+    cidade VARCHAR(250) NOT NULL,
+    estado VARCHAR(250) NOT NULL,
     cadastro TIMESTAMP WITHOUT TIME ZONE  NOT NULL,
-    nome_schema VARCHAR(250) NOT NULL
+    nome_schema VARCHAR(250) NOT NULL,
+    nr_cnpj VARCHAR(250) NOT NULL,
+    nm_tabeliao VARCHAR(250) NOT NULL,
+    pc_desconto_custa1 NUMERIC(12,2) NOT NULL DEFAULT 0,
+    pc_desconto_custa2 NUMERIC(12,2) NOT NULL DEFAULT 0,
+    pc_desconto_custa3 NUMERIC(12,2) NOT NULL DEFAULT 0,
+    pc_desconto_custa4 NUMERIC(12,2) NOT NULL DEFAULT 0,
+    pc_desconto_custa5 NUMERIC(12,2) NOT NULL DEFAULT 0,
+    pc_desconto_custa6 NUMERIC(12,2) NOT NULL DEFAULT 0,
+    pc_desconto_custa7 NUMERIC(12,2) NOT NULL DEFAULT 0,
+    pc_desconto_custa8 NUMERIC(12,2) NOT NULL DEFAULT 0,
+    pc_desconto_custa9 NUMERIC(12,2) NOT NULL DEFAULT 0,
+    pc_desconto_custa10 NUMERIC(12,2) NOT NULL DEFAULT 0
+
 );
 
 CREATE SCHEMA db_1;
 CREATE SCHEMA db_2;
 CREATE SCHEMA db_3;
 
-INSERT INTO tb_instituicao( nome, tipo_instituicao_id, cadastro,nome_schema) VALUES ('Cartorio',1, now(), 'db_1');
-INSERT INTO tb_instituicao( nome, tipo_instituicao_id, cadastro,nome_schema) VALUES ('Pessoa Fisica',1, now(), 'db_2');
-INSERT INTO tb_instituicao( nome, tipo_instituicao_id, cadastro,nome_schema) VALUES ('Cliente ',1, now(), 'db_3');
+INSERT INTO tb_instituicao( nome, cadastro, nome_schema, cep, logradouro, numero, bairro, cidade, estado, nr_cnpj,
+nm_tabeliao) VALUES ('Cartorio 1',now(), 'db_1','111380-100','logradouro 1','2','bairro','cidade', 'SP', '12.323.2323/12313-01', 'tabeliao 1');
+INSERT INTO tb_instituicao( nome, cadastro, nome_schema, cep, logradouro, numero, bairro, cidade, estado, nr_cnpj,
+nm_tabeliao) VALUES ('Cartorio 2', now(), 'db_2','111380-100','logradouro 2','3','bairro','cidade', 'SP', '12.323.2323/12313-01', 'tabeliao 2');
+INSERT INTO tb_instituicao( nome, cadastro, nome_schema, cep, logradouro, numero, bairro, cidade, estado, nr_cnpj,
+nm_tabeliao) VALUES ('Cartorio 3 ', now(), 'db_3','111380-100','logradouro 3','4','bairro','cidade', 'SP', '12.323.2323/12313-01', 'tabeliao 3');
--- src/main/resources/db/migration/public/V1_5__D_TipoInstituicao.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE tb_instituicao DROP COLUMN tipo_instituicao_id;
-DROP TABLE tb_tipo_instituicao;
